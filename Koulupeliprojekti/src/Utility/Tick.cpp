bf9683505a5871fa8309cb62827e7870153ab6f0
Valtis <erkka_kaaria@outlook.com>
2013-10-07 23:12:06 +0300

    Fixed an error in calculating tick length in Tick-class

--- Koulupeliprojekti/src/Utility/Tick.cpp
+++ Koulupeliprojekti/src/Utility/Tick.cpp
@@ -15,7 +15,7 @@ bool Tick::TickHasPassed()
   auto currentTick = SDL_GetTicks();
   if (currentTick > m_lastTick + m_tickLength)
   {
-    m_lastTickLength = currentTick - m_lastTickLength;
+    m_lastTickLength = currentTick - m_lastTick;
     m_lastTick = currentTick;
     return true;
   }
