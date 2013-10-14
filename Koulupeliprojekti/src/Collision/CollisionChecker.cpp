9e7a1e4415accae6d191dfa3e07e4cf9447029e7
Valtis <erkka_kaaria@outlook.com>
2013-10-14 18:13:27 +0300

    Added outline for collision check. Added a check so that collision isn't checked with itself.

--- Koulupeliprojekti/src/Collision/CollisionChecker.cpp
+++ Koulupeliprojekti/src/Collision/CollisionChecker.cpp
@@ -13,6 +13,16 @@ void CheckEntityCollision(const std::unique_ptr<Entity> &entity, const std::vect
 {
   for (auto &e : entities)
   {
-    
+    if (entity == e)
+    {
+      continue;
+    }
+    /*
+      Get location component and collision component from both entities
+      Check that the components aren't null
+      Determine if the collision areas overlap
+      Signal one or both entities that collision happened
+      If signaled both, make sure that same collision isn't checked twice
+    */
   }
 }
