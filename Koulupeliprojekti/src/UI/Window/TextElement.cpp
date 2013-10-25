f1c84b1d267cece2d9fc8e69c64c22dad519b555
Valtis <erkka_kaaria@outlook.com>
2013-10-25 12:28:08 +0300

    Changed textelement width/height calculations to query texture for its size instead of using approximative values

--- Koulupeliprojekti/src/UI/Window/TextElement.cpp
+++ Koulupeliprojekti/src/UI/Window/TextElement.cpp
@@ -2,11 +2,13 @@
 #include "Graphics/FontManager.h"
 TextElement::TextElement(std::string text, int fontSize, TextAlingment alignment, SDL_Rect location, Renderer *renderer)
 {
-  double width = FontManager::Instance().GetFontWidth(fontSize);
-  double height = FontManager::Instance().GetFontHeight(fontSize);
-
+  SDL_Texture *texture = FontManager::Instance().GetTextSingleLine(renderer, text, fontSize);
+  
+  int width = 0;
+  int height = 0;
+  SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+ 
   location.y = (location.h/2 - height/2); // align text so that it's halfway height wise
-
   switch (alignment)
   {
   case TextAlingment::ALIGN_LEFT:
@@ -19,11 +21,10 @@ TextElement::TextElement(std::string text, int fontSize, TextAlingment alignment
     location.x = location.w - width - 5;
     break;
   }
-
-  location.w = width*text.length();
+  location.w = width;
   location.h = height;
 
-  SetTexture(FontManager::Instance().GetTextSingleLine(renderer, text, fontSize));
+  SetTexture(texture);
 
   SetRect(location);
 }
