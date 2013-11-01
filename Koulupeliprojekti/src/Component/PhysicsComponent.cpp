05b3874033db9ff2198bd0f6b6abe18a9113fc1a
Janne Pakarinen <janne.pakarinen@pp2.inet.fi>
2013-11-01 12:25:56 +0200

    Bug fix

--- Koulupeliprojekti/src/Component/PhysicsComponent.cpp
+++ Koulupeliprojekti/src/Component/PhysicsComponent.cpp
@@ -48,18 +48,19 @@ float PhysicsComponent::GetTerminalVelocity()
 
 void PhysicsComponent::SetAcceleration(float x, float y)
 {
-  double _d, _x, _y;
-  if (x == 0 && y == 0)
+  if (x + y <= 1.0f && x + y >= -1.0f)
   {
     m_acceleration_x = x;
     m_acceleration_y = y;
-    return;
   }
+  else
+  {
+    double _d, _x, _y;
+    _x = static_cast<double>(x);
+    _y = static_cast<double>(y);
+    _d = _x + _y;
 
-  _x = static_cast<double>(x);
-  _y = static_cast<double>(y);
-  _d = _x + _y;
-
-  m_acceleration_x = static_cast<float>(_x / _d);
-  m_acceleration_y = static_cast<float>(_y / _d);
+    m_acceleration_x = static_cast<float>(_x / _d);
+    m_acceleration_y = static_cast<float>(_y / _d);
+  }
 }
