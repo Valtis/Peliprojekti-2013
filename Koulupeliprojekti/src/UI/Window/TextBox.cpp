fef09e5aac2d15338e343577f07375a0617084ae
Valtis <erkka_kaaria@outlook.com>
2013-11-02 13:11:22 +0200

    TextField class

--- Koulupeliprojekti/src/UI/Window/TextBox.cpp
+++ Koulupeliprojekti/src/UI/Window/TextBox.cpp
@@ -1,14 +1,16 @@
 #include "UI/Window/TextBox.h"
+#include "UI/Window/TextField.h"
 #include "Graphics/TextureFactory.h"
-
 TextBox::TextBox(SDL_Rect location, Renderer *renderer)
 {
   SetLocation(location);
   SDL_Color color = { 160, 160, 160, 255};
   SetTexture(TextureFactory::CreateWindowTexture(location.w, location.h, color, renderer));
 
- // std::unique_ptr<TextElement> element(new TextElement(text, 13, TextAlingment::ALIGN_CENTER, location, renderer));
-  //AddWindow(std::move(element));
+  std::string text = "This is a test string for testing purposes. This string\ncontains control characters\nso\tthat I see those working. There is also lots of text so I can see the text element works in general!";
+  
+  std::unique_ptr<TextField> element(new TextField(text, 13, location, renderer));
+  AddWindow(std::move(element));
 }
 
 TextBox::~TextBox()
---
+++ Koulupeliprojekti/src/UI/Window/TextField.cpp
@@ -0,0 +1,6 @@
+#include "UI/Window/TextField.h"
+
+TextField::TextField(std::string text, int fontSize, SDL_Rect location, Renderer *renderer)
+{
+
+}
