ac307fd9607f0ac5630347d89f36fd423a21c58e
Valtis <erkka_kaaria@outlook.com>
2013-11-27 19:31:29 +0200

    BlinkScript now handles blinking

--- Koulupeliprojekti/include/Component/Scripts/BlinkScript.h
+++ Koulupeliprojekti/include/Component/Scripts/BlinkScript.h
@@ -4,13 +4,23 @@ class BlinkScript : public Component
 {
 public:
   BlinkScript(double singleBlinkDuration) : m_blinkDuration(0), 
+    m_ticksUntilFlip(0), m_lastState(false),
     m_singleBlinkDuration(singleBlinkDuration) { }
   ~BlinkScript() { }
   void Update(double ticks) override;
+
+  void FlipGraphicsState();
+
+
+
 protected:
   void OnAttatchingToEntity() override;
 private:
+  void StopBlinking();
+  MessageHandling HandleStartBlinkingMessage(Message *message);
   double m_blinkDuration; // how long it keeps blinking
-  double m_singleBlinkDuration; // how long single frame is shown\hidden
+  double m_ticksUntilFlip;
+  bool m_lastState;
+  const double m_singleBlinkDuration; // how long single frame is shown\hidden
 
 };
--- Koulupeliprojekti/src/Component/Scripts/BlinkScript.cpp
+++ Koulupeliprojekti/src/Component/Scripts/BlinkScript.cpp
@@ -1,12 +1,62 @@
 #include "Component/Scripts/BlinkScript.h"
+#include "Message/StartBlinkingMessage.h"
+#include "Message/MessageProcessor.h"
 #include "Entity/Entity.h"
 void BlinkScript::OnAttatchingToEntity()
 {
-
+  GetOwner()->RegisterMessageHandler(MessageType::START_BLINKING, Priority::HIGH, 
+    [=](Message *msg)
+    {
+      return this->HandleStartBlinkingMessage(msg);
+    }
+    );
 }
 
 void BlinkScript::Update(double ticksPassed)
 {
+  if (m_blinkDuration > 0)
+  {
+    m_blinkDuration -= ticksPassed;
+    if (m_blinkDuration <= 0)
+    {
+      StopBlinking();
+      return;
+    }
 
+    m_ticksUntilFlip -= ticksPassed;
+    if (m_ticksUntilFlip <= 0)
+    {
+      FlipGraphicsState();
+
+    }
+  }
 }
 
+void BlinkScript::StopBlinking()
+{
+  auto msg = MessageFactory::CreateSetGraphicsVisibilityMessage(true);
+  GetOwner()->SendMessage(msg.get());
+
+  m_ticksUntilFlip = 0;
+  m_blinkDuration = 0;
+}
+
+
+void BlinkScript::FlipGraphicsState()
+{
+  m_ticksUntilFlip = m_singleBlinkDuration;
+  m_lastState = !m_lastState;
+  auto msg = MessageFactory::CreateSetGraphicsVisibilityMessage(m_lastState);
+  GetOwner()->SendMessage(msg.get());
+}
+
+
+MessageHandling BlinkScript::HandleStartBlinkingMessage(Message *message)
+{
+  auto blinkMsg = static_cast<StartBlinkingMessage *>(message);
+  m_blinkDuration = blinkMsg->GetBlinkDuration();
+  m_lastState = false;
+  auto msg = MessageFactory::CreateSetGraphicsVisibilityMessage(m_lastState);
+  GetOwner()->SendMessage(msg.get());
+  return MessageHandling::STOP_HANDLING;
+}
