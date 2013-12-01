245702238ca60bc8c2806451b032dbec9467bcd3
Husky <husky@paivola.fi>
2013-12-01 21:43:47 +0200

    moved keyboard binds to a file

--- Koulupeliprojekti/include/UI/Controller/KeyboardController.h
+++ Koulupeliprojekti/include/UI/Controller/KeyboardController.h
@@ -3,6 +3,7 @@
 #include <map>
 #include "UI/Controller/Controller.h"
 #include "Message/Commands/ControlCommand.h"
+#include <string>
 
 class KeyboardController : public Controller
 {
@@ -11,6 +12,7 @@ public:
 	Command* HandleInput(SDL_Event event) override;
 private:
 	void LoadBinds();
+	void AddBind(std::string, std::string);
 	std::map<SDL_Keycode, Action> m_binds;
 	int m_id;
 };
--- Koulupeliprojekti/src/UI/Controller/KeyboardController.cpp
+++ Koulupeliprojekti/src/UI/Controller/KeyboardController.cpp
@@ -1,6 +1,10 @@
 #include <SDL.h>
 #include "UI/Controller/KeyboardController.h"
 #include "Message/Commands/ControlCommand.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
 
 KeyboardController::KeyboardController(int id,bool active) : Controller(active), m_id(id)
 {
@@ -14,10 +18,10 @@ Command* KeyboardController::HandleInput(SDL_Event event)
 	if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
 	{
 		KeyState state = KeyState::UP; 
-    if (event.type == SDL_KEYDOWN)
-    {
-      state = KeyState::DOWN;
-    }
+	if (event.type == SDL_KEYDOWN)
+	{
+		state = KeyState::DOWN;
+	}
     
 		SDL_Keycode key = event.key.keysym.sym;
 		if (m_binds.count(key))
@@ -29,11 +33,85 @@ Command* KeyboardController::HandleInput(SDL_Event event)
 
 void KeyboardController::LoadBinds()
 {
+	std::ifstream file;
+	file.open("data/binds/binds.dat");
+	bool loaded = false;
+	if (file.is_open())
+	{
+		std::string line = "";
+		while (line != "[KEYBOARD]") std::getline(file,line);
+		while (!file.eof())
+		{
+			std::getline(file,line);
+			if (line == "")
+				break;
+			std::stringstream ssin(line);
+			std::string keycode, action;
+			ssin >> action;
+			ssin >> keycode;
+			ssin >> keycode;
+			std::transform(keycode.begin(),keycode.end(),keycode.begin(),::tolower);
+			std::transform(action.begin(),action.end(),action.begin(),::tolower);
+			AddBind(keycode, action);
+			loaded = true;
+		}
+		file.close();
+	}
+	if (loaded)
+		return;
 	m_binds[SDLK_LEFT] = Action::LEFT;
 	m_binds[SDLK_RIGHT] = Action::RIGHT;
 	m_binds[SDLK_SPACE] = Action::JUMP;
  	m_binds[SDLK_LSHIFT] = Action::FIRE;
 	m_binds[SDLK_ESCAPE] = Action::EXIT;
-  m_binds[SDLK_p] = Action::PAUSE;
-  m_binds[SDLK_PAUSE] = Action::PAUSE;
+ 	m_binds[SDLK_p] = Action::PAUSE;
+ 	m_binds[SDLK_PAUSE] = Action::PAUSE;
+}
+
+void KeyboardController::AddBind(std::string keycode, std::string actionstring)
+{
+	if (keycode.length() < 1 || actionstring.length() < 1)
+		return;
+	SDL_Keycode code;
+	Action action;
+	if (keycode =="left")
+		code = SDLK_LEFT;
+	else if (keycode =="right")
+		code = SDLK_RIGHT;
+	else if (keycode =="up")
+		code = SDLK_UP;
+	else if (keycode =="down")
+		code = SDLK_DOWN;
+	else if (keycode =="lshift")
+		code = SDLK_LSHIFT;
+	else if (keycode =="rshift")
+		code = SDLK_RSHIFT;
+	else if (keycode == "space")
+		code = SDLK_SPACE;
+	else if (keycode == "esc")
+		code = SDLK_ESCAPE;
+	else if (keycode.length() == 1)
+		code = static_cast<int>(keycode.c_str()[0]);
+	else
+		return;
+
+	if (actionstring == "shoot")
+		action = Action::FIRE;
+	else if (actionstring == "jump")
+		action = Action::JUMP;
+	else if (actionstring == "left")
+		action = Action::LEFT;
+	else if (actionstring == "right")
+		action = Action::RIGHT;
+	else if (actionstring == "up")
+		action = Action::UP;
+	else if (actionstring == "down")
+		action = Action::DOWN;
+	else if (actionstring == "exit")
+		action = Action::EXIT;
+	else if (actionstring == "pause")
+		action = Action::PAUSE;
+	else
+		return;
+	m_binds[code] = action;
 }
