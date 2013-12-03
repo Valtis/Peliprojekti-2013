556073dc512e177cd18833e13be5e0afe6a45da4
Janne <jtpakari@cs.helsinki.fi>
2013-12-03 15:49:34 +0200

    Classified collisioncheck

--- Koulupeliprojekti/include/Collision/CollisionChecker.h
+++ Koulupeliprojekti/include/Collision/CollisionChecker.h
@@ -1,10 +1,27 @@
 #pragma once
 #include <memory>
 #include <vector>
+#include <SDL.h>
+#include <SDL_thread.h>
+
 class Entity;
 
-namespace Collision
+class CollisionChecker
 {
-  void CheckCollisions(const std::vector<std::unique_ptr<Entity>> &entities,
-                       const std::vector<std::unique_ptr<Entity>> &staticEntities);
+  public:
+    CollisionChecker() { InitThreadPool(SDL_GetCPUCount()); };
+    ~CollisionChecker();
+
+    void CheckCollisions(const std::vector<std::unique_ptr<Entity>> &entities,
+                         const std::vector<std::unique_ptr<Entity>> &staticEntities);
+
+  private:
+    struct CollisionThread
+    {
+      SDL_Thread *thread;
+      int id;
+    };
+    std::vector<std::unique_ptr<CollisionThread>> m_threads;
+
+    InitThreadPool(int size);
 }
